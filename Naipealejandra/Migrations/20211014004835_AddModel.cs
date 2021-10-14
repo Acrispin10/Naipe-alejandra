40b7ac712acf4f3882e2d5e89d8500cfd70f7b29
LENOVO <LENOVO@LAPTOP-89OEER9H>
2021-10-13 21:13:41 -0400

    Agregar archivos de proyecto.

---
+++ Naipealejandra/Migrations/20211014004835_AddModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Naipealejandra.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Naipe",
+                columns: table => new
+                {
+                    IdNaipe = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: false),
+                    NombreNaipe = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    LinkNaipe = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Naipe", x => x.IdNaipe);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Naipe");
+        }
+    }
+}
---
+++ Naipealejandra/Models/Naipe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Naipealejandra.Models
+{
+    public class Naipe
+    {
+        [Key]
+        [Required]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "Por favor ingrese una sola letra")]
+        [Display(Name = "Inserte letra de la A a la K")]
+        public string IdNaipe { get; set; }
+
+        [Required] // Sea obligatorio
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "Por favor ingrese entre 2 y 60 caracteres")]
+        [Display(Name = "Nombre de la carta")]
+        public string NombreNaipe { get; set; }
+
+        [Url]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")] // Sea obligatorio
+        [Display(Name = "Link de la carta")]
+        public string LinkNaipe { get; set; }
+    }
+}
